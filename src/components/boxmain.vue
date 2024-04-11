54823fbf1c1e382a3f9621b2290f063d68f2365b
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-11 17:01:53 +0200

    fix: add spacing and remove one more div

--- src/components/boxmain.vue
+++ src/components/boxmain.vue
@@ -22,30 +22,30 @@
             <div class="container">
                 <div class="row">
                     <div class="ms-main-col-blue-area col py-3">
-                        <div class=" d-flex justify-content-center align-items-center">
-                            <div class="ms-img">
-                                <div>
-                                    <img src="../assets/img/buy-comics-digital-comics.png" alt="digital-comics">
-                                    <span class="text-white fw-bold">DIGITAL COMICS</span>
-                                </div>
-                                <div>
-                                    <img src="../assets/img/buy-comics-merchandise.png" alt="merch-comics">
-                                    <span class="text-white fw-bold">DC MERCHANDISE</span>
-                                </div>
-                                <div>
-                                    <img src="../assets/img/buy-comics-subscriptions.png" alt="sub-comics">
-                                    <span class="text-white fw-bold">SUBSCRIPTION</span>
-                                </div>
-                                <div>
-                                    <img src="../assets/img/buy-comics-shop-locator.png" alt="shop-comics">
-                                    <span class="text-white fw-bold">COMIC SHOP LOCATOR</span>
-                                </div>
-                                <div>
-                                    <img src="../assets/img/buy-dc-power-visa.svg" alt="shop-comics">
-                                    <span class="text-white fw-bold">DC POWER VISA</span>
+                        <div class="d-flex ms-img align-items-center">
+                            <div>
+                                <img src="../assets/img/buy-comics-digital-comics.png" alt="digital-comics">
+                                <span class="text-white fw-bold">DIGITAL COMICS</span>
+                            </div>
 
-                                </div>
+                            <div>
+                                <img src="../assets/img/buy-comics-merchandise.png" alt="merch-comics">
+                                <span class="text-white fw-bold">DC MERCHANDISE</span>
+                            </div>
 
+                            <div>
+                                <img src="../assets/img/buy-comics-subscriptions.png" alt="sub-comics">
+                                <span class="text-white fw-bold">SUBSCRIPTION</span>
+                            </div>
+
+                            <div>
+                                <img src="../assets/img/buy-comics-shop-locator.png" alt="shop-comics">
+                                <span class="text-white fw-bold">COMIC SHOP LOCATOR</span>
+                            </div>
+
+                            <div>
+                                <img src="../assets/img/buy-dc-power-visa.svg" alt="shop-comics">
+                                <span class="text-white fw-bold">DC POWER VISA</span>
                             </div>
                         </div>
                     </div>
@@ -69,8 +69,8 @@
             align-items: center;
 
             img {
-                margin: 0 10px;
-                width: 80px;
+                margin: 0 0 0 11px;
+                width: 60px;
             }
         }
     }
