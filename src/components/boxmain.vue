db80d515788ee6e2af22855f6b760ec3193071d4
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-11 17:09:42 +0200

    fix: add col class to single div

--- src/components/boxmain.vue
+++ src/components/boxmain.vue
@@ -20,33 +20,31 @@
 
         <section class="ms-blue-box d-flex align-items-center">
             <div class="container">
-                <div class="row">
-                    <div class="ms-main-col-blue-area col py-3">
-                        <div class="ms-img">
-                            <div>
-                                <img src="../assets/img/buy-comics-digital-comics.png" alt="digital-comics" class="m-0">
-                                <span class="text-white fw-bold">DIGITAL COMICS</span>
-                            </div>
+                <div class="row ms-main-col-blue-area py-3">
+                    <div class="ms-img">
+                        <div class="col">
+                            <img src="../assets/img/buy-comics-digital-comics.png" alt="digital-comics" class="ms-0">
+                            <span class="text-white fw-bold">DIGITAL COMICS</span>
+                        </div>
 
-                            <div>
-                                <img src="../assets/img/buy-comics-merchandise.png" alt="merch-comics">
-                                <span class="text-white fw-bold">DC MERCHANDISE</span>
-                            </div>
+                        <div class="col">
+                            <img src="../assets/img/buy-comics-merchandise.png" alt="merch-comics">
+                            <span class="text-white fw-bold">DC MERCHANDISE</span>
+                        </div>
 
-                            <div>
-                                <img src="../assets/img/buy-comics-subscriptions.png" alt="sub-comics">
-                                <span class="text-white fw-bold">SUBSCRIPTION</span>
-                            </div>
+                        <div class="col">
+                            <img src="../assets/img/buy-comics-subscriptions.png" alt="sub-comics">
+                            <span class="text-white fw-bold">SUBSCRIPTION</span>
+                        </div>
 
-                            <div>
-                                <img src="../assets/img/buy-comics-shop-locator.png" alt="shop-comics">
-                                <span class="text-white fw-bold">COMIC SHOP LOCATOR</span>
-                            </div>
+                        <div class="col">
+                            <img src="../assets/img/buy-comics-shop-locator.png" alt="shop-comics">
+                            <span class="text-white fw-bold">COMIC SHOP LOCATOR</span>
+                        </div>
 
-                            <div>
-                                <img src="../assets/img/buy-dc-power-visa.svg" alt="shop-comics">
-                                <span class="text-white fw-bold">DC POWER VISA</span>
-                            </div>
+                        <div class="col">
+                            <img src="../assets/img/buy-dc-power-visa.svg" alt="shop-comics">
+                            <span class="text-white fw-bold">DC POWER VISA</span>
                         </div>
                     </div>
                 </div>
@@ -69,8 +67,12 @@
             align-items: center;
 
             img {
-                margin: 0 0 0 11px;
-                width: 60px;
+                margin: 0 10px 0 11px;
+                width: 50px;
+            }
+
+            span {
+                font-size: 15px;
             }
         }
     }
