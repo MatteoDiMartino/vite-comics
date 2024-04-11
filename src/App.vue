fb0e27bbdb04fb15e6ea9dc68783a14d2b4b325c
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-11 14:41:13 +0200

    feat: add base code test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
-<script setup>
-import header from './components/header.vue'
+<script>
+
+  import AppTitle from './components/AppTitle.vue'
+
+  export default {
+    components: {
+      AppTitle
+    }
+  }
+
 </script>
 
 <template>
 
+  <AppTitle></AppTitle>
+
 </template>
 
-<style scoped lang="scss">
-
+<style lang="scss">
+  @use './style/generic.scss'
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,41 @@
+<script>
+    export default {
+        name: 'AppTitle'
+    }
+</script>
+
+<template>
+
+<header>
+    <h1>ciao</h1>
+    <ul>
+        <li>
+            <a href="#">link</a>
+        </li>
+        
+        <li>
+            <a href="#">link</a>
+        </li>
+        
+        <li>
+            <a href="#">link</a>
+        </li>
+        
+        <li>
+            <a href="#">link</a>
+        </li>
+        
+        <li>
+            <a href="#">link</a>
+        </li>
+        
+        <li>
+            <a href="#">link</a>
+        </li>
+    </ul>
+</header>
+
+</template>
+
+
+<style scoped lang="scss"></style>
