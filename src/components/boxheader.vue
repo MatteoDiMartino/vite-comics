bbce35f97f36253db694e336da87d7a03e9939d0
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-11 16:00:19 +0200

    fix: move img folder and add logo

--- src/components/boxheader.vue
+++ src/components/boxheader.vue
@@ -8,11 +8,28 @@ export default {
 
 <template>
 
-<header class="bg-danger d-flex justify-content-center fs-2 text-white fw-bold">
-    ciao
+<header class="bg-white d-flex justify-content-center fs-2 fw-bold">
+    <div class="container">
+        <div class="row">
+            <div class="col ms-box1header">
+                <img src="../assets/img/dc-logo.png" alt="dc-logo" class="py-2">
+            </div>
+            <div class="col ms-box2header"></div>
+        </div>
+    </div>
 </header>
 
 </template>
 
 <style scoped lang="scss">
+
+.ms-box1header {
+
+    img {
+        width: 90px;
+    }
+
+}
+
+
 </style>
